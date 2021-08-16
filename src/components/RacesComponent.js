5e7d16fb7b6f78d25fe54b0b2357bd982cbd2986
Christoph Lansche <c.lansche@web.de>
2021-08-16 18:26:43 +0200

    Redesign Card: push card-link to bottom, place country-flag beside date.

--- src/components/RacesComponent.js
+++ src/components/RacesComponent.js
@@ -16,23 +16,32 @@ class Races extends Component {
     render() {
         const race = this.props.sportEvents.map((sportEvent) => {
           return (
-            <div key={sportEvent.id} className="col-12 col-md-4 m-1">
-              <Card>
+            <div key={sportEvent.id} className="col-12 col-md-4">
+              <Card className="h-100">
                 <CardBody>
                   <div className="clickable" onClick={() => this.props.onClick(sportEvent.id)}>
-                    <CardImg width="100%" src={"assets/images/event-visuals/" + sportEvent.visual} alt="" />
+                    <CardImg width="100%" src={"assets/images/event-visuals/" + (sportEvent.visual)} alt="" />
                     <CardImgOverlay>
                       <CardTitle>
                         <div className="milky-background sport-event-name">
-                        <h6>{sportEvent.start} {sportEvent.end ? ( <span> - {sportEvent.end}</span>) : null}</h6>{/**renders sportEvent.end only if it exists and != null */}
-                        <h5 className="">
-                          {sportEvent.name}
-                        </h5>                        
-                      </div>{/** /.milky-background sport-event-name */}
-                      </CardTitle>                   
+                          <h6>
+                            <div className="row">
+                              <span className="col-10">
+                                {sportEvent.start} {sportEvent.end ? (<span> - {sportEvent.end}</span>) : null}{/**renders sportEvent.end only if it exists and != null */}
+                              </span>
+                              <span className="col-2">
+                                <img className="img-fluid" src={"assets/images/country-flags/svg/" + (sportEvent.countryCode).toUpperCase() + ".svg"} alt="" align="absmiddle" />
+                              </span>
+                            </div>
+                          </h6>
+                          <h5>
+                            {sportEvent.name}
+                          </h5>
+                        </div>{/** /.milky-background sport-event-name */}
+                      </CardTitle>
                     </CardImgOverlay>                                   
                     <CardText>                     
-                      <h6>{sportEvent.host}</h6>
+                      <h6 className="mt-2">{sportEvent.host}</h6>
                       <p><small>in {sportEvent.postalCode} {sportEvent.city}</small></p>
                       <hr />
                       <ul className="list-unstyled">
@@ -44,7 +53,7 @@ class Races extends Component {
                       </ul>                    
                     </CardText>
                   </div>{/** /.clickable */}                  
-                  <a className="card-link text-decoration-none" href={sportEvent.homepage} target="_blank" rel="noreferrer"><FontAwesomeIcon icon="globe" /> Homepage</a>
+                  <a className="mt-auto card-link text-decoration-none" href={sportEvent.homepage} target="_blank" rel="noreferrer"><FontAwesomeIcon icon="globe" /> Homepage</a>
                 </CardBody>
               </Card>
             </div>/** /key=sportEvent.id */
@@ -56,7 +65,9 @@ class Races extends Component {
             <div className="row">
               <h1>Veranstaltungskalender</h1>
               <hr />
-              {race}
+              <div className="row row-cols-1 row-cols-md-3 g-4">
+                {race}
+              </div>              
             </div>
           </div>
         );
--- src/shared/sportEvents.js
+++ src/shared/sportEvents.js
@@ -77,6 +77,139 @@ export const SPORTEVENTS = [
     end: "",
     city: "Hattersheim",
     postalCode: "65000",
+    countryCode: "AT",
+    createdBy: "",
+    maintainedBy: [
+      {
+        userId: "",
+      },
+      {
+        userId: "",
+      },
+    ],
+    races: [
+      {
+        id: 0,
+        name: "Staufen Bike kurz",
+        sport: "bike",
+        endurance: "",
+        start: "30.03.2021",
+        competition: false,
+        courses: [
+          {
+            type: "MTB",
+            distance: "20",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Staufen Bike mittel",
+        sport: "bike",
+        endurance: "",
+        start: "30.03.2021",
+        competition: false,
+        courses: [
+          {
+            type: "MTB",
+            distance: "40",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Staufen Bike lang",
+        sport: "bike",
+        endurance: "",
+        start: "30.03.2021",
+        competition: false,
+        courses: [
+          {
+            type: "MTB",
+            distance: "60",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Ladenburger Triathlonfestival",
+    host: "TV Ladenburg",
+    visual: "ladenburger_triathlon_festival.jpg",
+    homepage: "https://roemerman.de/",
+    start: "17.07.2021",   
+    end: "19.07.2021",
+    city: "Ladenburg",
+    postalCode: "68526",
+    countryCode: "DE",
+    createdBy: "",
+    maintainedBy: [
+      {
+        userId: "",
+      },
+      {
+        userId: "",
+      },
+    ],
+    races: [
+      {
+        id: 0,
+        name: "Fitnesstriathlon",
+        sport: "Triathlon",
+        endurance: "Sprint distance",
+        start: "17.07.2021",
+        competition: true,
+        courses: [
+          {
+            type: "swim",
+            distance: "0,5",
+          },
+          {
+            type: "TT",
+            distance: "20",
+          },
+          {
+            type: "run",
+            distance: "5",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Römerman",
+        sport: "Triathlon",
+        endurance: "Short distance",
+        start: "17.07.2012",
+        competition: true,
+        courses: [
+          {
+            type: "swim",
+            distance: "1,8",
+          },
+          {
+            type: "TT",
+            distance: "42",
+          },
+          {
+            type: "run",
+            distance: "10",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "Staufen Bike",
+    host: "RSV Hattersheim",
+    visual: "staufen_bike.jpg",
+    homepage: "https://www.rc-hattersheim.de/de/home/",
+    start: "30.03.2021",
+    end: "",
+    city: "Hattersheim",
+    postalCode: "65000",
     countryCode: "CH",
     createdBy: "",
     maintainedBy: [
